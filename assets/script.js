9ce82238616bcf51abdde4b02964324bccbe2a59
coyote <megan.metelak@gmail.com>
2022-04-23 11:15:14 -0500

    re-define variables

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,18 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Array for lowercase alphabet
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// Array for uppercase alphabet
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+// Array for symbols
+var symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|","<",",",">",".","?","/","'",'"',":",","]
+// Array for numbers
+var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+// Array for final password
+var password = []
 
 // Write password to the #password input
 function writePassword() {
